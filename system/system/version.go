f7c39b2c8665cbd90641137ad4daa1bdf8835eed
ip <ivan.poluianov@gmail.com>
2023-07-18 10:43:12 +0200

    version

--- system/system/version.go
+++ system/system/version.go
@@ -1,4 +1,4 @@
-package system
-
-const BUILDTIME = "2023-07-17"
-const VERSION = "2.4.9"
+package system 
+ 
+const BUILDTIME = "10:42:57.32 Tue 07/18/2023" 
+const VERSION = "2.4.9" 
--- update_version.bat
+++ update_version.bat
@@ -3,4 +3,4 @@
 ECHO package system > system/system/version.go
 ECHO. >> system/system/version.go
 ECHO const BUILDTIME = "%time% %date%" >> system/system/version.go
-ECHO const VERSION = "2.4.4" >> system/system/version.go
+ECHO const VERSION = "2.4.9" >> system/system/version.go
